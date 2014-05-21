56da12d266bd318893fee305724c62436fdcb63b
Jarva <fp.jarva@gmail.com>
2014-05-21 11:53:55 +0100

    Added Linux Compatibility

---
+++ models/medieval/pbed.bat
@@ -0,0 +1,4 @@
+@echo on
+cd "C:\Program Files (x86)\Steam\steamapps\chaos880\sourcesdk\bin\orangebox\bin>"
+studiomdl.exe "C:\Program Files (x86)\Steam\steamapps\chaos880\half-life 2 episode two\ep2\models\medieval\\pbed.qc"
+@pause
