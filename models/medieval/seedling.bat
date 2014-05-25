b591321485ef995fa2629c20aa5f73bb6f060881
Jarva <fp.jarva@gmail.com>
2014-05-25 13:43:23 +0100

    Removed useless files

--- models/medieval/seedling.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-cd "C:\Program Files (x86)\Steam\steamapps\chaos880\sourcesdk\bin\orangebox\bin>"
-studiomdl.exe "C:\Program Files (x86)\Steam\steamapps\chaos880\half-life 2 episode two\ep2\models\medieval\seedling.qc"
-@pause
